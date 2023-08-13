d99fac35cf6b4bf4fc687cf8874cf81219b364cb
Charlotte <139652782+64BitKosher@users.noreply.github.com>
2023-08-13 14:46:03 -0500

    Update Program.cs
    added some namespace stu

--- Program.cs
+++ Program.cs
@@ -10,8 +10,7 @@ namespace Dump2Cs
         {
             string inputFileContent = File.ReadAllText("dump.cs");
 
-            Console.WriteLine("Do you want to remove comments from generated files? (y/n)");
-            bool removeComments = Console.ReadLine().ToLower() == "y";
+            inputFileContent = Regex.Replace(inputFileContent, @"//.*|/\*(.|\n)*?\*/", "");
 
             MatchCollection classMatches = Regex.Matches(inputFileContent, @"class\s+[A-Za-z_]\w*\s*{[^}]*}");
 
@@ -26,40 +25,87 @@ namespace Dump2Cs
             foreach (Match match in classMatches)
             {
                 string classContent = match.Value;
+                string namespaceName = Regex.Match(classContent, @"namespace\s+([A-Za-z_]\w*)").Groups[1].Value;
                 string className = Regex.Match(classContent, @"class\s+([A-Za-z_]\w*)").Groups[1].Value;
 
-                string newClassContent = $@"
-using System;
+                if (string.IsNullOrEmpty(namespaceName))
+                {
+                    namespaceName = "NoNamespace";
+                }
 
-namespace Dump2Cs
-{{
-    {RemoveCommentsFromCode(classContent, removeComments)}
-}}
-";
-
-                File.WriteAllText($"{className}.cs", newClassContent);
-                Console.WriteLine($"Generated {className}.cs");
+                GenerateClassFile(classContent, namespaceName, className, true);
+                Console.WriteLine($"Generated {namespaceName}.{className}.cs");
                 classNumber++;
             }
 
             Console.WriteLine("Files generated successfully.");
             WaitForEnterKey();
         }
+        static void ProcessNamespace(string namespaceName, string namespaceContent, bool removeComments)
+        {
+            if (string.IsNullOrEmpty(namespaceName))
+            {
+                namespaceName = "NoNamespace";
+            }
+
+            string[] lines = namespaceContent.Split('\n');
+            foreach (string line in lines)
+            {
+                if (line.Trim().StartsWith("// Namespace:"))
+                {
+                    namespaceName = line.Trim().Replace("// Namespace:", "").Trim();
+                    break;
+                }
+            }
+
+            MatchCollection classMatches = Regex.Matches(namespaceContent, @"class\s+[A-Za-z_]\w*\s*{[^}]*}");
+
+            if (classMatches.Count == 0)
+            {
+                Console.WriteLine($"No class content found for namespace: {namespaceName}");
+                return;
+            }
+
+            foreach (Match classMatch in classMatches)
+            {
+                string classContent = classMatch.Value;
+                string className = Regex.Match(classContent, @"class\s+([A-Za-z_]\w*)").Groups[1].Value;
+
+                GenerateClassFile(classContent, namespaceName, className, removeComments);
+            }
+        }
+
+        static void GenerateClassFile(string classContent, string namespaceName, string className, bool removeComments)
+        {
+            if (!string.IsNullOrEmpty(classContent))
+            {
+                string newClassContent = classContent;
+                if (removeComments)
+                {
+                    newClassContent = Regex.Replace(classContent, @"//.*|/\*(.|\n)*?\*/", "");
+                }
+
+                string fullNamespace = !string.IsNullOrEmpty(namespaceName) ? $"{namespaceName}." : string.Empty;
+                string fileName = $"{fullNamespace}{className}.cs";
+
+                string newClassFileContent = $@"
+using System;
+
+namespace {fullNamespace}{{
+    {newClassContent}
+}}
+";
+
+                File.WriteAllText(fileName, newClassFileContent);
+                Console.WriteLine($"Generated {fileName}");
+            }
+        }
+
 
         static void WaitForEnterKey()
         {
             Console.WriteLine("Press Enter to exit...");
             while (Console.ReadKey().Key != ConsoleKey.Enter) { }
-        }
-
-        static string RemoveCommentsFromCode(string code, bool removeComments)
-        {
-            if (removeComments)
-            {
-                code = Regex.Replace(code, @"//.*", "");
-                code = Regex.Replace(code, @"/\*(.|\n)*?\*/", "");
-            }
-            return code;
         }
     }
 }
